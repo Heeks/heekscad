ed52f051602a2d7b1e7f6bcb2058df24d4d16b47
Dan Heeks <danheeks@gmail.com>
2008-07-01 21:26:29 +0000

    added a few more interface functions

--- interface/HeeksCADInterface.h
+++ interface/HeeksCADInterface.h
@@ -21,10 +21,12 @@ public:
 	virtual void AddToolBarButton(wxToolBar* toolbar, const wxString& title, wxBitmap& bitmap, const wxString& caption, void(*onButtonFunction)(wxCommandEvent&));
 	virtual wxString GetExeFolder();
 	virtual void AddUndoably(HeeksObj* object);
-	virtual const std::list<HeeksObj*>& GetSelection();
+	virtual const std::list<HeeksObj*>& GetMarkedList();
 	virtual bool GetArcDirection(HeeksObj* object);
 	virtual bool GetArcCentre(HeeksObj* object, double* c);
 	virtual bool GetArcAxis(HeeksObj* object, double* a);
 	virtual void get_2d_arc_segments(double xs, double ys, double xe, double ye, double xc, double yc, bool dir, bool want_start, double pixels_per_mm, void(*callbackfunc)(const double* xy));
 	virtual double GetPixelScale();
+	virtual void Mark(HeeksObj* object);
+	virtual void ClearMarkedList();
 };
