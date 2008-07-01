3689e47967196f515636da6cc5c60a066e6ddbad
Dan Heeks <danheeks@gmail.com>
2008-07-01 10:56:42 +0000

    I changed the interface to be loads of virtual functions instead of having to do GetSymbol on each of them.

--- src/Interface.h
+++ src/Interface.h
@@ -1,15 +1,7 @@
 // declares all the exported functions for HeeksCAD
 
+class CHeeksCADInterface;
+
 extern "C"{
-#define HEEKS_EXPORT __declspec( dllexport ) __cdecl
-
-class wxFrame;
-class wxAuiManager;
-
-double	HEEKS_EXPORT	HeeksGetTolerance(void);
-void	HEEKS_EXPORT	HeeksRefreshProperties(void);
-void	HEEKS_EXPORT	HeeksRepaint(void);
-int		HEEKS_EXPORT	HeeksGetMainFrame(void);
-int		HEEKS_EXPORT	HeeksGetAuiManager();
-void	HEEKS_EXPORT	HeeksAddToolBarTool(wxToolBar* toolbar, const wxString& title, wxBitmap& bitmap, const wxString& caption, void(*onButtonFunction)(wxCommandEvent&));
+__declspec( dllexport ) CHeeksCADInterface* __cdecl HeeksCADGetInterface(void);
 }
