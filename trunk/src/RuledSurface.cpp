92556bb29475f39f0e5023368d773546c582b8a5
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2008-09-25 22:22:21 +0000

    I've added an extrude button

--- trunk/src/RuledSurface.cpp
+++ trunk/src/RuledSurface.cpp
@@ -3,6 +3,7 @@
 #include "stdafx.h"
 #include "RuledSurface.h"
 #include "Wire.h"
+#include "Face.h"
 #include "ConversionTools.h"
 #include "MarkedList.h"
 
@@ -46,6 +47,55 @@ void PickCreateRuledSurface()
 	}
 }
 
+void PickCreateExtrusion()
+{
+	if(wxGetApp().m_marked_list->size() == 0)
+	{
+		wxGetApp().PickObjects("pick a wire, face or line/arc collection");
+	}
+
+	if(wxGetApp().m_marked_list->size() > 0)
+	{
+		std::list<TopoDS_Shape> wire_or_face_list;
+
+		for(std::list<HeeksObj *>::const_iterator It = wxGetApp().m_marked_list->list().begin(); It != wxGetApp().m_marked_list->list().end(); It++)
+		{
+			HeeksObj* object = *It;
+			switch(object->GetType())
+			{
+			case WireType:
+				wire_or_face_list.push_back(((CWire*)object)->Wire());
+				break;
+
+			case FaceType:
+				wire_or_face_list.push_back(((CFace*)object)->Face());
+				break;
+
+			case LineArcCollectionType:
+				{
+					std::list<HeeksObj*> list;
+					list.push_back(object);
+					TopoDS_Wire wire;
+					if(ConvertLineArcsToWire2(list, wire))
+					{
+						wire_or_face_list.push_back(wire);
+					}
+				}
+				break;
+
+			}
+		}
+
+		TopoDS_Shape shape;
+		if(CreateExtrusion(wire_or_face_list, shape))
+		{
+			HeeksObj* new_object = CShape::MakeObject(shape, "Extruded Solid");
+			wxGetApp().AddUndoably(new_object, NULL, NULL);
+			wxGetApp().Repaint();
+		}
+	}
+}
+
 bool CreateRuledSurface(const std::list<TopoDS_Wire> &wire_list, TopoDS_Shape& shape)
 {
 	if(wire_list.size() > 0)
@@ -57,6 +107,19 @@ bool CreateRuledSurface(const std::list<TopoDS_Wire> &wire_list, TopoDS_Shape& s
 			generator.AddWire(wire);
 		}
 
+		generator.Build();
+		shape = generator.Shape();
+
+		return true;
+	}
+	return false;
+}
+
+bool CreateExtrusion(const std::list<TopoDS_Shape> &wire_or_face_list, TopoDS_Shape& shape)
+{
+	if(wire_or_face_list.size() > 0)
+	{
+		BRepPrimAPI_MakePrism generator( wire_or_face_list.front(), gp_Vec(0, 0, 1) );
 		generator.Build();
 		shape = generator.Shape();
 
--- trunk/src/RuledSurface.h
+++ trunk/src/RuledSurface.h
@@ -1,4 +1,6 @@
 // RuledSurface.h
 
 extern void PickCreateRuledSurface();
+extern void PickCreateExtrusion();
 extern bool CreateRuledSurface(const std::list<TopoDS_Wire> &wire_list, TopoDS_Shape& shape);
+extern bool CreateExtrusion(const std::list<TopoDS_Shape> &wire_or_face_list, TopoDS_Shape& shape);
