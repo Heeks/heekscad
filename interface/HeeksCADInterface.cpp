14a44cd01179022b7a1831d250ee47cb7266b707
Dan Heeks <danheeks@gmail.com>
2008-10-21 07:19:12 +0000

    added a view rotate button

--- interface/HeeksCADInterface.cpp
+++ interface/HeeksCADInterface.cpp
@@ -235,9 +235,9 @@ void CHeeksCADInterface::WereRemoved(const std::list<HeeksObj*> &list)
 	wxGetApp().WereRemoved(list);
 }
 
-int CHeeksCADInterface::PickObjects(const wxChar* str)
+int CHeeksCADInterface::PickObjects(const wxChar* str, bool m_just_one)
 {
-	return wxGetApp().PickObjects(str);
+	return wxGetApp().PickObjects(str, m_just_one);
 }
 
 bool CHeeksCADInterface::PickPosition(const wxChar* str, double* pos)
--- src/Face.cpp
+++ src/Face.cpp
@@ -322,6 +322,7 @@ gp_Dir CFace::GetMiddleNormal(gp_Pnt *pos)const{
 }
 
 gp_Dir CFace::GetNormalAtUV(double u, double v, gp_Pnt *pos)const{
+	if(m_topods_face.IsNull()) return gp_Dir(0, 0, 0);
 	Handle(Geom_Surface) surf=BRep_Tool::Surface(m_topods_face);          // get surface properties
 	GeomLProp_SLProps props(surf, u, v, 1, 0.01);          // get surface normal
 	gp_Dir norm=props.Normal();                         // check orientation
