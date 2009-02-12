d081875c4402fb1b461672eb92a8fde5171b3d43
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-02-12 16:25:35 +0000

    fixed some compiler warnings

--- trunk/src/HLine.cpp
+++ trunk/src/HLine.cpp
@@ -303,3 +303,4 @@ void HLine::Reverse()
 	A = B;
 	B = temp;
 }
+
--- trunk/src/Sketch.h
+++ trunk/src/Sketch.h
@@ -10,8 +10,8 @@ enum SketchOrderType
 	SketchOrderTypeEmpty,	// an empty sketch
 	SketchOrderTypeOpen,	// a well ordered sketch, which doesn't have it's start and end touching
 	SketchOrderTypeReverse,	// reverse the sketch, only used as a function, won't be returned by GetSketchOrder()
-	SketchOrderTypeBad,		// "badly ordered" ( where the start of one span doesn't line up with the end of the previous span )
-	SketchOrderTypeGood,	// a well ordered sketch, open/close unknown, won't be returned by GetSketchOrder()
+	SketchOrderTypeBad,	// "badly ordered" ( where the start of one span doesn't line up with the end of the previous span )
+	SketchOrderTypeReOrder,	// re-order the sketch, only used as a function, won't be returned by GetSketchOrder()
 	SketchOrderTypeCloseCW,	// a well ordered sketch, which is closed and clockwise
 	SketchOrderTypeCloseCCW,// a well ordered sketch, which is closed and anti-clockwise
 	MaxSketchOrderTypes
