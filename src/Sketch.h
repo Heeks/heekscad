0d598087b33c6dca2cf3fbb72dd8628dbc56c17e
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-02-22 22:31:29 +0000

    work on HeeksCNC Issue 42:  	 Pocketing, re-order sketch

--- src/Sketch.h
+++ src/Sketch.h
@@ -4,18 +4,7 @@
 
 #include "../interface/ObjList.h"
 #include "../interface/HeeksColor.h"
-
-enum SketchOrderType
-{
-	SketchOrderTypeEmpty,	// an empty sketch
-	SketchOrderTypeOpen,	// a well ordered sketch, which doesn't have it's start and end touching
-	SketchOrderTypeReverse,	// reverse the sketch, only used as a function, won't be returned by GetSketchOrder()
-	SketchOrderTypeBad,	// "badly ordered" ( where the start of one span doesn't line up with the end of the previous span )
-	SketchOrderTypeReOrder,	// re-order the sketch, only used as a function, won't be returned by GetSketchOrder()
-	SketchOrderTypeCloseCW,	// a well ordered sketch, which is closed and clockwise
-	SketchOrderTypeCloseCCW,// a well ordered sketch, which is closed and anti-clockwise
-	MaxSketchOrderTypes
-};
+#include "../interface/SketchOrder.h"
 
 class CSketch:public ObjList
 {
