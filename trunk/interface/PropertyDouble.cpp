555850d6819d1f3c8dca95c64579749e8fae88cf
jonpry <jonpry@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-04-22 23:56:36 +0000

    Fixed spline movement/handles. Added some new constructors to support stl solids

--- trunk/interface/PropertyDouble.cpp
+++ trunk/interface/PropertyDouble.cpp
@@ -9,8 +9,21 @@
 PropertyDouble::PropertyDouble(const wxChar* t, double initial_value, HeeksObj* object, void(*callbackfunc)(double, HeeksObj*)):Property(){
 	m_initial_value = initial_value;
 	m_callbackfunc = callbackfunc;
+	m_callbackfuncidx = 0;
 	m_object = object;
 	title = wxString(t);
+	has_index = false;
+}
+
+PropertyDouble::PropertyDouble(const wxChar* t, double initial_value, HeeksObj* object, void(*callbackfunc)(double, HeeksObj*, int), int index):Property()
+{
+	m_initial_value = initial_value;
+	m_callbackfuncidx = callbackfunc;
+	m_callbackfunc = 0;
+	m_object = object;
+	title = wxString(t);
+	has_index = true;
+	m_index = index;
 }
 
 PropertyDouble::~PropertyDouble(){
@@ -23,4 +36,10 @@ const wxChar* PropertyDouble::GetShortString(void)const{
 Property *PropertyDouble::MakeACopy(void)const{
 	PropertyDouble* new_object = new PropertyDouble(*this);
 	return new_object;
+}
+
+void PropertyDouble::CallSetFunction() const
+{ 
+	if(m_callbackfunc)(*m_callbackfunc)(m_initial_value, m_object);
+	if(m_callbackfuncidx)(*m_callbackfuncidx)(m_initial_value, m_object,m_index);
 }
--- trunk/interface/PropertyVertex.cpp
+++ trunk/interface/PropertyVertex.cpp
@@ -9,7 +9,19 @@ PropertyVertex::PropertyVertex(const wxChar *t, const double *initial_vt, HeeksO
 	title = wxString(t);
 	memcpy(m_x, initial_vt, 3*sizeof(double));
 	m_callbackfunc = callbackfunc;
+	m_callbackfuncidx = 0;
 	m_object = object;
+	has_index = false;
+}
+
+PropertyVertex::PropertyVertex(const wxChar *t, const double *initial_vt, HeeksObj* object, void(*callbackfunc)(const double* vt, HeeksObj* m_object, int), int index){
+	title = wxString(t);
+	memcpy(m_x, initial_vt, 3*sizeof(double));
+	m_callbackfunc = 0;
+	m_callbackfuncidx = callbackfunc;
+	m_object = object;
+	has_index = true;
+
 }
 
 PropertyVertex::~PropertyVertex(){
@@ -22,4 +34,9 @@ const wxChar* PropertyVertex::GetShortString()const{
 Property *PropertyVertex::MakeACopy(void)const{
 	PropertyVertex* new_object = new PropertyVertex(*this);
 	return new_object;
+}
+
+void PropertyVertex::CallSetFunction() const{
+	if(m_callbackfunc)(*m_callbackfunc)(m_x, m_object);
+	if(m_callbackfuncidx)(*m_callbackfuncidx)(m_x,m_object,m_index);
 }
