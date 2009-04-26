ad4786a760bb05d5b25c868a51b4c80f028743e7
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-04-26 13:56:08 +0000

    forgot to add Group.h and .cpp

---
+++ trunk/src/Group.h
@@ -0,0 +1,18 @@
+// Group.h
+// Copyright (c) 2009, Dan Heeks
+// This program is released under the BSD license. See the file COPYING for details.
+
+#pragma once
+
+#include "../interface/ObjList.h"
+
+class CGroup: public ObjList{
+public:
+	const wxChar* GetTypeString(void)const{return _("Group");}
+	int GetType()const{return GroupType;}
+	HeeksObj *MakeACopy(void)const{ return new CGroup(*this);}
+	wxString GetIcon(){return wxGetApp().GetExeFolder() + _T("/icons/group");}
+	void WriteXML(TiXmlNode *root);
+
+	static HeeksObj* ReadFromXMLElement(TiXmlElement* pElem);
+};
