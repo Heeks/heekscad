938f5b64a3f385ddb39a5cb1935649f7fe49e031
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-04-26 13:56:08 +0000

    forgot to add Group.h and .cpp

---
+++ src/Group.cpp
@@ -0,0 +1,23 @@
+// Group.cpp
+// Copyright (c) 2009, Dan Heeks
+// This program is released under the BSD license. See the file COPYING for details.
+
+#include "stdafx.h"
+#include "Group.h"
+#include "../tinyxml/tinyxml.h"
+
+void CGroup::WriteXML(TiXmlNode *root)
+{
+	TiXmlElement * element = new TiXmlElement( "Group" );  
+	root->LinkEndChild( element );
+	WriteBaseXML(element);
+}
+
+// static member function
+HeeksObj* CGroup::ReadFromXMLElement(TiXmlElement* pElem)
+{
+	CGroup* new_object = new CGroup;
+	new_object->ReadBaseXML(pElem);
+
+	return (ObjList*)new_object;
+}
