cb804f9d2e22019dcf2a3c8da71d746f98ffd5d3
jonpry <jonpry@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-06-12 18:32:51 +0000

    Added code for ellipse drawing

--- src/DigitizedPoint.h
+++ src/DigitizedPoint.h
@@ -33,6 +33,7 @@ public:
 	static bool GetLinePoints(const DigitizedPoint& d1, const DigitizedPoint& d2, gp_Pnt &p1, gp_Pnt &p2);
 	static bool GetArcPoints(const DigitizedPoint& d1, const gp_Vec *initial_direction, const DigitizedPoint& d2, gp_Pnt &p1, gp_Pnt &p2, gp_Pnt &centre, gp_Dir &axis);
 	static bool GetTangentCircle(const DigitizedPoint& d1, const DigitizedPoint& d2, const DigitizedPoint& d3, gp_Circ& c);
+	static bool GetEllipse(const DigitizedPoint& d1, const DigitizedPoint& d2, const DigitizedPoint& d3, gp_Elips& e);
 	static bool GetCircleBetween(const DigitizedPoint& d1, const DigitizedPoint& d2, gp_Circ& c);
 };
 
