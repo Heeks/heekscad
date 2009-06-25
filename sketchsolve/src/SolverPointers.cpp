681546542c4208f9caa23f0d07902e7b57a227e0
geo01005 <geo01005@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-06-25 01:02:25 +0000

    Made the Hessian inverse estimate positive definite.

--- sketchsolve/src/SolverPointers.cpp
+++ sketchsolve/src/SolverPointers.cpp
@@ -99,7 +99,7 @@ int main() {
 		pparameters[i] = &parameters[i];
 	}
 
-	sol=solve(pparameters ,8,cons,3,fine);
+	sol=solve(pparameters ,8,cons,3,rough);
 	if(sol==succsess)
 	{
 		cout<<"A good Solution was found"<<endl;
