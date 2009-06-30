7ef0e77a1ab750350d0a1a2df173f1869cf9319e
jonpry <jonpry@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-06-30 21:24:10 +0000

    Added matrix.cpp

---
+++ src/matrix.cpp
@@ -0,0 +1,195 @@
+// matrix.cpp
+// Copyright (c) 2009, Dan Heeks
+// This program is released under the BSD license. See the file COPYING for details.
+
+#include "stdafx.h"
+#include "matrix.h"
+
+Matrix::Matrix(int n, int m)
+{
+	m_n = n;
+	m_m = m;
+	m_temp = false;
+	m_data = new double[n*m];
+}
+
+Matrix::~Matrix()
+{
+	if(!m_temp)
+		delete m_data;
+}
+
+inline
+double Matrix::GetElement(unsigned row, unsigned col)
+{
+	return m_data[m_n*row + col];
+}
+
+inline
+double& Matrix::operator() (unsigned row, unsigned col)
+{
+   return m_data[m_n*row + col];
+}
+ 
+inline
+double Matrix::operator() (unsigned row, unsigned col) const
+{
+   return m_data[m_n*row + col];
+} 
+
+Matrix Matrix::Multiplied(Matrix* mat)
+{
+	if(m_m != mat->m_n)
+		return Matrix(0,0);
+
+	Matrix nmat(m_n,mat->m_m);
+
+	for(int i=0; i < m_n; i++)
+	{
+		for(int j=0; j < m_m; j++)
+		{
+			nmat(i,j) = 0;
+			for(int k=0; k < mat->m_m; k++)
+			{
+				nmat(i,j) += GetElement(i,j) * mat->GetElement(j,k);
+			}
+		}
+	}
+	return nmat;
+}
+
+Matrix Matrix::Multiplied(double s)
+{
+	Matrix nmat(m_n,m_m);
+
+	for(int i=0; i < m_n; i++)
+	{
+		for(int j=0; j < m_m; j++)
+		{
+			nmat(i,j) = s * GetElement(i,j);
+		}
+	}
+	return nmat;
+}
+
+void Matrix::Multiply(Matrix *mat)
+{
+	Matrix m = Multiplied(mat);
+	m.m_temp = true;
+	m_m = m.m_m;
+	m_n = m.m_n;
+	delete m_data;
+	m_data = m.m_data;
+}
+
+void Matrix::Multiply(double s)
+{
+	Matrix m = Multiplied(s);
+	m.m_temp = true;
+	m_m = m.m_m;
+	m_n = m.m_n;
+	delete m_data;
+	m_data = m.m_data;
+}
+
+void Matrix::Add(Matrix* mat)
+{
+	Matrix m = Added(mat);
+	m.m_temp = true;
+	m_m = m.m_m;
+	m_n = m.m_n;
+	delete m_data;
+	m_data = m.m_data;
+}
+
+void Matrix::Subtract(Matrix* mat)
+{
+	Matrix m = Subtracted(mat);
+	m.m_temp = true;
+	m_m = m.m_m;
+	m_n = m.m_n;
+	delete m_data;
+	m_data = m.m_data;
+}
+
+void Matrix::Subtract(double s)
+{
+	Matrix m = Subtracted(s);
+	m.m_temp = true;
+	m_m = m.m_m;
+	m_n = m.m_n;
+	delete m_data;
+	m_data = m.m_data;
+}
+
+void Matrix::Add(double s)
+{
+	Matrix m = Added(s);
+	m.m_temp = true;
+	m_m = m.m_m;
+	m_n = m.m_n;
+	delete m_data;
+	m_data = m.m_data;
+}
+
+Matrix Matrix::Added(Matrix *mat)
+{
+	if(m_m != mat->m_m || m_n != mat->m_n)
+		return Matrix(0,0);
+
+	Matrix nmat(m_n,m_m);
+	for(int i=0; i < m_n; i++)
+	{
+		for(int j=0; j < m_m; j++)
+		{
+			nmat(i,j) = GetElement(i,j) + mat->GetElement(i,j);
+		}
+	}
+
+	return nmat;
+}
+
+Matrix Matrix::Subtracted(Matrix *mat)
+{
+	if(m_m != mat->m_m || m_n != mat->m_n)
+		return Matrix(0,0);
+
+	Matrix nmat(m_n,m_m);
+	for(int i=0; i < m_n; i++)
+	{
+		for(int j=0; j < m_m; j++)
+		{
+			nmat(i,j) = GetElement(i,j) - mat->GetElement(i,j);
+		}
+	}
+
+	return nmat;
+}
+
+Matrix Matrix::Subtracted(double s)
+{
+	Matrix nmat(m_n,m_m);
+	for(int i=0; i < m_n; i++)
+	{
+		for(int j=0; j < m_m; j++)
+		{
+			nmat(i,j) = GetElement(i,j) - s;
+		}
+	}
+
+	return nmat;
+}
+
+Matrix Matrix::Added(double s)
+{
+	Matrix nmat(m_n,m_m);
+	for(int i=0; i < m_n; i++)
+	{
+		for(int j=0; j < m_m; j++)
+		{
+			nmat(i,j) = GetElement(i,j) + s;
+		}
+	}
+
+	return nmat;
+}
---
+++ src/matrix.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+class Matrix
+{
+	double* m_data;
+	int m_n;
+	int m_m;
+	bool m_temp;
+public:
+	Matrix(int n, int m);
+	~Matrix();
+	double& operator() (unsigned row, unsigned col); 
+	double  operator() (unsigned row, unsigned col) const;
+	//double Matrix::operator() (unsigned row, unsigned col);
+	double GetElement(unsigned row, unsigned col);
+	Matrix Multiplied(Matrix* mat);
+	void Multiply(Matrix* mat);
+	Matrix Multiplied(double s);
+	void Multiply(double s);
+	Matrix Added(double s);
+	Matrix Added(Matrix *mat);
+	void Add(double s);
+	void Add(Matrix *mat);
+	Matrix Subtracted(double s);
+	Matrix Subtracted(Matrix *mat);
+	void Subtract(Matrix* mat);
+	void Subtract(double s);
+};
