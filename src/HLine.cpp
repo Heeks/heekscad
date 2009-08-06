ca1495f5ce639f825f68569299040e9f8dce86f5
David.Nicholls.homeip.net <David.Nicholls.homeip.net@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-08-06 05:56:24 +0000

    Fixed problem where, if the imported line was of zero length, OpenCascade would throw a construction error when creating a gp_Vec frmo it.  If a line is of zero length, we discard the object when reading in the line object.

--- src/HLine.cpp
+++ src/HLine.cpp
@@ -168,6 +168,14 @@ void HLine::GetProperties(std::list<Property *> *list){
 }
 
 bool HLine::FindNearPoint(const double* ray_start, const double* ray_direction, double *point){
+	// The OpenCascade libraries throw an exception when one tries to
+	// create a gp_Lin() object using a vector that doesn't point
+	// anywhere.  If this is a zero-length line then we're in
+	// trouble.  Don't bother with it.
+	if ((A->m_p.X() == B->m_p.X()) &&
+	    (A->m_p.Y() == B->m_p.Y()) &&
+	    (A->m_p.Z() == B->m_p.Z())) return(false);
+
 	gp_Lin ray(make_point(ray_start), make_vector(ray_direction));
 	gp_Pnt p1, p2;
 	ClosestPointsOnLines(GetLine(), ray, p1, p2);
@@ -333,6 +341,14 @@ HeeksObj* HLine::ReadFromXMLElement(TiXmlElement* pElem)
 		else if(name == "ey"){p1.SetY(a->DoubleValue());}
 		else if(name == "ez"){p1.SetZ(a->DoubleValue());}
 	}
+
+	// The OpenCascade libraries throw an exception when one tries to
+	// create a gp_Lin() object using a vector that doesn't point
+	// anywhere.  If this is a zero-length line then we're in
+	// trouble.  Don't bother with it.
+	if ((p0.X() == p1.X()) &&
+	    (p0.Y() == p1.Y()) &&
+	    (p0.Z() == p1.Z())) return(NULL);
 
 	HLine* new_object = new HLine(p0, p1, &c);
 	new_object->ReadBaseXML(pElem);
