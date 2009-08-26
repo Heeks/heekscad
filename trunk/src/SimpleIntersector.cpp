ce435cc4d6987809119750118749bd83c85f7933
jonpry <jonpry@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-08-26 02:18:16 +0000

    Some work on arc support in multipoly.

--- trunk/src/SimpleIntersector.cpp
+++ trunk/src/SimpleIntersector.cpp
@@ -25,12 +25,16 @@ std::map<FastCurve*, std::vector<Intersection> > SimpleIntersector::Intersect(st
 	{
 		for(unsigned j=i+1; j < lines.size(); j++)
 		{
-			IntResult ir = Intersects(lines[i],lines[j]);
-
-			if(ir.exists)
+			std::vector<IntResult> results = Intersects(lines[i],lines[j]);
+			
+			for(int k=0; k < results.size(); k++)
 			{
-				intersections[lines[i]].push_back(Intersection(lines[j],ir.atX,ir.atY));
-				intersections[lines[j]].push_back(Intersection(lines[i],ir.atX,ir.atY));
+				IntResult ir = results[k];
+				if(ir.exists)
+				{
+					intersections[lines[i]].push_back(Intersection(lines[j],ir.atX,ir.atY));
+					intersections[lines[j]].push_back(Intersection(lines[i],ir.atX,ir.atY));
+				}
 			}
 		}
 	}
@@ -45,20 +49,22 @@ std::map<FastCurve*, std::vector<Intersection> > SimpleIntersector::Intersect(st
 	return intersections;
 }
 
-IntResult SimpleIntersector::Intersects(FastCurve* curve1, FastCurve* curve2)
+std::vector<IntResult> SimpleIntersector::Intersects(FastCurve* curve1, FastCurve* curve2)
 {
+	std::vector<IntResult> ret;
+	bool swap=false;
 	FastLine* line1 = dynamic_cast<FastLine*>(curve1);
 	if(line1)
 	{
 		FastLine* line2 = dynamic_cast<FastLine*>(curve2);
 		if(line2)
-			return LinesIntersect((FastLine*)curve1,(FastLine*)curve2);
+			ret = LinesIntersect((FastLine*)curve1,(FastLine*)curve2);
 		else
 		{
 			FastArc* arc2 = dynamic_cast<FastArc*>(curve2);
 			if(arc2)
 			{
-				return LineArcIntersect(line1,arc2);
+				ret = LineArcIntersect(line1,arc2);
 			}
 		}
 	}
@@ -70,7 +76,8 @@ IntResult SimpleIntersector::Intersects(FastCurve* curve1, FastCurve* curve2)
 			FastLine* line2 = dynamic_cast<FastLine*>(curve2);
 			if(line2)
 			{
-				return LineArcIntersect(line2,arc1);
+				ret = LineArcIntersect(line2,arc1);
+				swap = true;
 			}
 			else
 			{
@@ -79,11 +86,20 @@ IntResult SimpleIntersector::Intersects(FastCurve* curve1, FastCurve* curve2)
 		}
 	}
 
-	return IntResult(false,0,0,0,0);
+	if(swap)
+	{
+		for(int i=0; i < ret.size(); i++)
+		{
+			ret[i].Swap();
+		}
+	}
+
+	return ret;
 }
 
-IntResult SimpleIntersector::LinesIntersect(FastLine* line1, FastLine* line2)
+std::vector<IntResult> SimpleIntersector::LinesIntersect(FastLine* line1, FastLine* line2)
 {
+	std::vector<IntResult> ret;
 	double ua = (line2->B.X()-line2->A.X())*(line1->A.Y()-line2->A.Y()) - (line2->B.Y()-line2->A.Y())*(line1->A.X()-line2->A.X());
 	ua /= (line2->B.Y()-line2->A.Y())*(line1->B.X()-line1->A.X())-(line2->B.X()-line2->A.X())*(line1->B.Y()-line1->A.Y());
 	double ub = (line1->B.X()-line1->A.X())*(line1->A.Y()-line2->A.Y()) - (line1->B.Y()-line1->A.Y())*(line1->A.X()-line2->A.X());
@@ -93,30 +109,55 @@ IntResult SimpleIntersector::LinesIntersect(FastLine* line1, FastLine* line2)
 	{
 		double atX = line1->A.X() + ua * (line1->B.X() - line1->A.X());
 		double atY = line1->A.Y() + ua * (line1->B.Y() - line1->A.Y());
-		return IntResult(true,ua,ub,atX,atY);
+		ret.push_back(IntResult(true,ua,ub,atX,atY));
 	}
-	return IntResult(false,0,0,0,0);
+	return ret;
 }
 
-IntResult SimpleIntersector::LineArcIntersect(FastLine* line, FastArc* arc)
+std::vector<IntResult> SimpleIntersector::LineArcIntersect(FastLine* line, FastArc* arc)
 {
+	std::vector<IntResult> ret;
 
 	double dx = line->B.X() - line->A.X();
 	double dy = line->B.Y() - line->A.Y();
 	double a = dx*dx + dy*dy;
 	double b = 2 * ((line->B.X() - line->A.X()) * (line->A.X() - arc->C.X()) + (line->B.Y() - line->A.Y()) * (line->A.Y() - arc->C.Y())); 
 	double c = arc->C.X() * arc->C.X() + arc->C.Y() * arc->C.Y() + line->A.X()*line->A.X() + line->A.Y()*line->A.Y() - 2*(arc->C.X() * line->A.X() + arc->C.Y() * line->A.Y()) - arc->rad * arc->rad;
-/*	
-	double ua = (line2->B.X()-line2->A.X())*(line1->A.Y()-line2->A.Y()) - (line2->B.Y()-line2->A.Y())*(line1->A.X()-line2->A.X());
-	ua /= (line2->B.Y()-line2->A.Y())*(line1->B.X()-line1->A.X())-(line2->B.X()-line2->A.X())*(line1->B.Y()-line1->A.Y());
-	double ub = (line1->B.X()-line1->A.X())*(line1->A.Y()-line2->A.Y()) - (line1->B.Y()-line1->A.Y())*(line1->A.X()-line2->A.X());
-	ub /= (line2->B.Y()-line2->A.Y())*(line1->B.X()-line1->A.X())-(line2->B.X()-line2->A.X())*(line1->B.Y()-line1->A.Y());
 
-	if(ua > -tol && ua < 1+tol && ub > -tol && ub < 1+tol)
+	double det = b * b - 4 * a * c;
+	if(det<0)
+		return ret;
+
+	if(det > -tol && det < tol)
 	{
-		double atX = line1->A.X() + ua * (line1->B.X() - line1->A.X());
-		double atY = line1->A.Y() + ua * (line1->B.Y() - line1->A.Y());
-		return IntResult(true,ua,ub,atX,atY);
-	}*/
-	return IntResult(false,0,0,0,0);
+		//single intersection
+		double u = (-b + sqrt(det)) / (2 * a);
+		ret.push_back(LineArcIntersect(line,arc,u));
+		return ret;
+	}
+
+	//These u's are for the line
+	double u1 = (-b + sqrt(det)) / (2 * a);
+	double u2 = (-b - sqrt(det)) / (2 * a);
+
+	ret.push_back(LineArcIntersect(line,arc,u1));
+	ret.push_back(LineArcIntersect(line,arc,u2));
+
+	return ret;
+}
+
+IntResult SimpleIntersector::LineArcIntersect(FastLine* line, FastArc* arc, double u)
+{
+	if(u < -tol || u > 1 + tol)
+		return IntResult(false,0,0,0,0);
+
+	double x = line->GetXatU(u);
+	double y = line->GetYatU(u);
+
+	double arcu = arc->GetU(x,y);
+
+	if(arcu < -tol || arcu > 1 + tol)
+		return IntResult(false,0,0,0,0);
+
+	return IntResult(true,u,arcu,x,y);
 }
