f53b0c6b83c62de158506397bbf94506eef50ce0
jonpry <jonpry@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-08-29 07:02:01 +0000

    Multipoly working with arcs, except for the random crashing.

--- trunk/src/SimpleIntersector.h
+++ trunk/src/SimpleIntersector.h
@@ -7,6 +7,8 @@ public:
 	std::map<FastCurve*, std::vector<Intersection> > Intersect(std::vector<FastCurve*> &lines);
 	std::vector<IntResult> LinesIntersect(FastLine* line1, FastLine* line2);
 	std::vector<IntResult> LineArcIntersect(FastLine* line, FastArc* arc);
+	std::vector<IntResult> ArcsIntersect(FastArc* arc1, FastArc* arc2);
+	IntResult ArcsIntersect(FastArc* arc1, FastArc* arc2, double x, double y);
 	IntResult LineArcIntersect(FastLine* line, FastArc* arc, double u);
 	std::vector<IntResult> Intersects(FastCurve* curve1, FastCurve* curve2);
 };
