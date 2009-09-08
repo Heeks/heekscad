bd6ced0220fc9931b0d47bcf025e3ec9bdcf7526
jonpry <jonpry@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-09-08 22:56:02 +0000

    Pad sort of kind of works

---
+++ src/SketchTools.h
@@ -0,0 +1,7 @@
+// SketchTools.h
+// Copyright (c) 2009, Dan Heeks
+// This program is released under the BSD license. See the file COPYING for details.
+#include "../interface/Tool.h"
+
+void GetSketchMenuTools(std::list<Tool*>* t_list);
+
