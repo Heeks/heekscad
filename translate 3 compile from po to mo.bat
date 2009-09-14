36fefe1b7926e31a425fd42c2971a9ce6f846673
Dan Heeks <danheeks@gmail.com>
2009-09-14 22:18:53 +0000

    preparing for making a Windows installation again.

--- translate 3 compile from po to mo.bat
+++ translate 3 compile from po to mo.bat
@@ -1,7 +1,6 @@
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\de\HeeksCAD.mo" "%HEEKSCADPATH%\de\HeeksCAD.po"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\zh_CN\HeeksCAD.mo" "%HEEKSCADPATH%\zh_CN\HeeksCAD.po"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\zh_TW\HeeksCAD.mo" "%HEEKSCADPATH%\zh_TW\HeeksCAD.po"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\ru\HeeksCAD.mo" "%HEEKSCADPATH%\ru\HeeksCAD.po"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\ru\ru.mo" "%HEEKSCADPATH%\ru\ru.po"
+mkdir "%HEEKSCADPATH%\de"
+"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\de\HeeksCAD.mo" "%HEEKSCADPATH%\translations\de\HeeksCAD.po"
+mkdir "%HEEKSCADPATH%\it"
+"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\it\HeeksCAD.mo" "%HEEKSCADPATH%\translations\it\HeeksCAD.po"
 
 pause
