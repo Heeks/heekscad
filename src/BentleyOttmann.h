dc3a706cdc658c404df93a9538c965382a37060d
David.Nicholls.homeip.net <David.Nicholls.homeip.net@1e116681-dd50-0410-83da-2bc3025dbda1>
2009-10-30 12:34:22 +0000

    Moved the EventType enumeration to within the BentleyOttmann class so as to distinguish it from that defined in the UndoEngine.  Added the class context prefix to the enumeration names within the CPP file.

--- src/BentleyOttmann.h
+++ src/BentleyOttmann.h
@@ -2,7 +2,6 @@
 // Copyright (c) 2009, Dan Heeks
 // This program is released under the BSD license. See the file COPYING for details.
 
-
 #pragma once
 
 #include "Intersector.h"
@@ -10,19 +9,20 @@
 class BentleyOttmann: public Intersector{
 public:
 	std::map<FastCurve*, std::vector<Intersection> > Intersect(std::vector<FastCurve*> &lines);
-};
 
-enum EventType
-{
-	AddType,
-	RemoveType,
-	AddRemoveType,
-	IntersectionType
+	enum EventType
+	{
+		AddType,
+		RemoveType,
+		AddRemoveType,
+		IntersectionType
+	};
 };
 
 bool MyIsEqual(double a, double b);
 bool MyIsEqual2(FastLine* line1, FastLine* line2, double at);
 double MyRound(double d);
 IntResult Intersects(FastLine* line1, FastLine* line2);
-void InsertEvent(EventType type, double x, FastLine* line);
+void InsertEvent(BentleyOttmann::EventType type, double x, FastLine* line);
+
 
