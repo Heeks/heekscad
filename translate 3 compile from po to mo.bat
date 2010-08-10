9ab8e778bc09ee8c6f10b820c83c753df91f0f6c
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2010-08-10 11:33:46 +0000

    updating the Windows installation files to add slovak language

--- translate 3 compile from po to mo.bat
+++ translate 3 compile from po to mo.bat
@@ -4,5 +4,7 @@ mkdir "%HEEKSCADPATH%\it"
 "C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\it\HeeksCAD.mo" "%HEEKSCADPATH%\translations\it\HeeksCAD.po"
 mkdir "%HEEKSCADPATH%\fr"
 "C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\fr\HeeksCAD.mo" "%HEEKSCADPATH%\translations\fr\HeeksCAD.po"
+mkdir "%HEEKSCADPATH%\sk"
+"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\sk\HeeksCAD.mo" "%HEEKSCADPATH%\translations\sk\HeeksCAD.po"
 
 pause
