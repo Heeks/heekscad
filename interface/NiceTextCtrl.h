2282c515ab5b85e183d207ab42936aaf320d851d
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2010-09-23 21:58:40 +0000

    changes to allow optional editing of objects with dialogs

---
+++ interface/NiceTextCtrl.h
@@ -0,0 +1,31 @@
+// NiceTextCtrl.h
+// Copyright (c) 2010, Dan Heeks
+// This program is released under the BSD license. See the file COPYING for details.
+
+// a dialog control to enter a number
+class CDoubleCtrl: public wxTextCtrl
+{
+	wxString DoubleToString(double value);
+	double m_factor;
+public:
+	CDoubleCtrl(wxWindow* parent, wxWindowID id, double factor = 1.0);
+	double GetValue();
+	void SetValue(double value);
+};
+
+// a dialog control to enter a number in the current units
+class CLengthCtrl: public CDoubleCtrl
+{
+public:
+	CLengthCtrl(wxWindow* parent, wxWindowID id);
+};
+
+// a dialog control to enter a list of ids, for sketches or solids
+class CObjectIdsCtrl: public wxTextCtrl
+{
+public:
+	CObjectIdsCtrl(wxWindow* parent, wxWindowID id);
+	void GetAddChildren(HeeksObj* object, int group_type);
+	void SetFromChildren(HeeksObj* object, int group_type);
+};
+
