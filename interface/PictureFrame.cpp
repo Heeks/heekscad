05660f17e898a0c1ce882b541ac93305d920fe56
Dan Heeks <danheeks@gmail.com>
2010-09-30 15:20:31 +0000

    changes for loading a picture in PictureWindow

--- interface/PictureFrame.cpp
+++ interface/PictureFrame.cpp
@@ -22,19 +22,34 @@ BEGIN_EVENT_TABLE(PictureFrame,wxScrolledWindow)
     EVT_PAINT(PictureFrame::OnPaint)
 END_EVENT_TABLE()
 
-PictureWindow::PictureWindow(wxWindow* parent, const wxBitmap& b): wxWindow(parent, wxID_ANY, wxDefaultPosition, wxSize(b.GetWidth(), b.GetHeight())), m_bitmap(b){
+PictureWindow::PictureWindow(wxWindow* parent, const wxSize& size): wxWindow(parent, wxID_ANY, wxDefaultPosition, size), m_bitmap_set(false)
+{
+}
+
+PictureWindow::PictureWindow(wxWindow* parent, const wxBitmap& b): wxWindow(parent, wxID_ANY, wxDefaultPosition, wxSize(b.GetWidth(), b.GetHeight())), m_bitmap(b), m_bitmap_set(true)
+{
 }
 
 void PictureWindow::OnPaint(wxPaintEvent &event) {
 	wxPaintDC dc(this);
 	PrepareDC(dc);
-	dc.DrawBitmap(m_bitmap, 0,0, false);
+	if(m_bitmap_set)
+	{
+		dc.DrawBitmap(m_bitmap, 0,0, false);
+	}
 }
 
 void PictureWindow::SetPicture(const wxBitmap& b)
 {
 	m_bitmap = b;
+	if(!m_bitmap_set)m_bitmap_set = true;
 	Refresh();
+}
+
+void PictureWindow::SetPicture(wxBitmap** b, const wxString& filepath, long image_type)
+{
+	if(*b == NULL)*b = new wxBitmap(wxImage(filepath, image_type));
+	SetPicture(**b);
 }
 
 BEGIN_EVENT_TABLE(PictureWindow,wxWindow)
