1b78ef99c5129b1cfd0c62a97b56f5fbb26c6504
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2010-11-25 20:34:46 +0000

    I separated the Heeks-specific code from dxf.cpp and dxf.h, into HDxf.cpp and HDxf.h, so that dxf.cpp and dxf.h can be reused in other projects.

---
+++ trunk/src/HDxf.h
@@ -0,0 +1,40 @@
+// HDxf.h
+// Copyright (c) 2010, Dan Heeks
+// This program is released under the BSD license. See the file COPYING for details.
+
+#pragma once
+
+#include "dxf.h"
+
+class CSketch;
+
+class HeeksDxfRead : public CDxfRead{
+private:
+    typedef wxString LayerName_t;
+	typedef std::map< LayerName_t, CSketch * > Sketches_t;
+	Sketches_t m_sketches;
+
+	HeeksColor DecodeACI(const int aci);
+	void OnReadSpline(TColgp_Array1OfPnt &control, TColStd_Array1OfReal &weight, TColStd_Array1OfReal &knot,TColStd_Array1OfInteger &mult, int degree, bool periodic, bool rational);
+
+protected:
+	HeeksColor *ActiveColorPtr(Aci_t & aci);
+
+public:
+	HeeksDxfRead(const wxChar* filepath);
+
+	static bool m_make_as_sketch;
+	static bool m_ignore_errors;
+
+	// CDxfRead's virtual functions
+	void OnReadLine(const double* s, const double* e);
+	void OnReadPoint(const double* s);
+	void OnReadText(const double* point, const double height, const wxString text);
+	void OnReadArc(const double* s, const double* e, const double* c, bool dir);
+	void OnReadCircle(const double* s, const double* c, bool dir);
+    void OnReadEllipse(const double* c, double major_radius, double minor_radius, double rotation, double start_angle, double end_angle, bool dir);
+	void OnReadSpline(struct SplineData& sd);
+
+	void AddObject(HeeksObj *object);
+	void AddGraphics() const;
+};
