c9628ba949c9e2e0d1894f5f797597512a29a7d4
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2011-02-14 09:32:05 +0000

    changed version to 0.17.0. Also, fixed a complie warning.

--- trunk/src/HeeksCAD.cpp
+++ trunk/src/HeeksCAD.cpp
@@ -104,7 +104,7 @@ HeeksCADapp::HeeksCADapp(): ObjList()
 	_CrtSetAllocHook(MyAllocHook);
 #endif
 
-	m_version_number = _T("0 16 0");
+	m_version_number = _T("0 17 0");
 	m_geom_tol = 0.000001;
 	TiXmlBase::SetRequiredDecimalPlaces( DecimalPlaces(m_geom_tol) );	 // Ensure we write XML in enough accuracy to be useful when re-read.
 
