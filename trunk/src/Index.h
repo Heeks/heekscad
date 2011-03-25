ac2a52e75c5f2ea143967d879664140e7dda0960
jcoffland@gmail.com <jcoffland@gmail.com@1e116681-dd50-0410-83da-2bc3025dbda1>
2011-03-25 01:02:12 +0000

    Fixed a crash when selecting faces, Small update to index algorithm.

--- trunk/src/Index.h
+++ trunk/src/Index.h
@@ -29,17 +29,14 @@ public:
     // This is essentially the same algorithm SQLite uses for choosing indices.
 
     // First try the next number in order.  Most of the time this will work.
-    INDEX_T x = next++;
-    if (index.find(x) == index.end()) return do_insert(x, o);
+    if (!next) next++;  // Skip zero
+    if (index.find(next) == index.end()) return do_insert(o);
 
     // If 'next' is in use then do a random search.  Things will slow down if 
-    // we start running out of indices, which normall shouldn't happen.
+    // we start running out of indices, which normally wont happen.
     while (true) {
-      x = rand() + 1;
-      for (unsigned i = 0; i < sizeof(INDEX_T) / 2; i++)
-        x *= rand() + 1;
-
-      if (index.find(x) == index.end()) return do_insert(x, o);
+      next = (next << 15) + rand();
+      if (next && index.find(next) == index.end()) return do_insert(o);
     }
   }
 
@@ -47,8 +44,8 @@ public:
   void clear() {next = 1; index.clear();}
 
 protected:
-  INDEX_T do_insert(INDEX_T x, OBJECT_T o) {
-    index.insert(typename index_t::value_type(x, o));
-    return x;
+  INDEX_T do_insert(OBJECT_T o) {
+    index.insert(typename index_t::value_type(next, o));
+    return next++;
   }
 };
