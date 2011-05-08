d26365097045f66c78c232b330ab868e5f52c995
Dan Heeks <danheeks@gmail.com>
2011-05-08 19:21:58 +0000

    I added "custom grippers" property to a group object, so that I can choose where a group of solids rotates about when dragged. This makes it possible to show how a hinged assembly will work.

--- src/Group.cpp
+++ src/Group.cpp
@@ -5,11 +5,38 @@
 #include "stdafx.h"
 #include "Group.h"
 #include "Shape.h"
+#include "../interface/PropertyCheck.h"
+#include "../interface/PropertyVertex.h"
+
+CGroup::CGroup()
+{
+	m_title = _("Group");
+	m_gripper_datum_set = false;
+	m_custom_grippers = false;
+}
 
 void CGroup::WriteXML(TiXmlNode *root)
 {
 	TiXmlElement * element = new TiXmlElement( "Group" );
 	root->LinkEndChild( element );
+    element->SetAttribute("title", m_title.utf8_str());
+	element->SetAttribute("custom_grippers", m_custom_grippers ? 1:0);
+	element->SetAttribute("gripper_datum_set", m_gripper_datum_set ? 1:0);
+	if(m_gripper_datum_set)
+	{
+		element->SetDoubleAttribute("ox", m_o.X());
+		element->SetDoubleAttribute("oy", m_o.Y());
+		element->SetDoubleAttribute("oz", m_o.Z());
+		element->SetDoubleAttribute("pxx", m_px.X());
+		element->SetDoubleAttribute("pxy", m_px.Y());
+		element->SetDoubleAttribute("pxz", m_px.Z());
+		element->SetDoubleAttribute("pyx", m_py.X());
+		element->SetDoubleAttribute("pyy", m_py.Y());
+		element->SetDoubleAttribute("pyz", m_py.Z());
+		element->SetDoubleAttribute("pzx", m_pz.X());
+		element->SetDoubleAttribute("pzy", m_pz.Y());
+		element->SetDoubleAttribute("pzz", m_pz.Z());
+	}
 
 	// instead of ObjList::WriteBaseXML(element), write the id of solids, or the object
 	std::list<HeeksObj*>::iterator It;
@@ -34,6 +61,31 @@ HeeksObj* CGroup::ReadFromXMLElement(TiXmlElement* element)
 	CGroup* new_object = new CGroup;
 
 	// instead of ( ObjList:: ) new_object->ReadBaseXML(pElem);
+
+	if(element->Attribute("title"))new_object->m_title = Ctt(element->Attribute("title"));
+	int int_for_bool;
+	if(element->Attribute("custom_grippers", &int_for_bool))new_object->m_custom_grippers = (int_for_bool != 0);
+	if(element->Attribute("gripper_datum_set", &int_for_bool))new_object->m_gripper_datum_set = (int_for_bool != 0);
+	if(new_object->m_gripper_datum_set)
+	{
+		double o[3], px[3], py[3], pz[3];
+		element->Attribute("ox", &o[0]);
+		element->Attribute("oy", &o[1]);
+		element->Attribute("oz", &o[2]);
+		new_object->m_o = make_point(o);
+		element->Attribute("pxx", &px[0]);
+		element->Attribute("pxy", &px[1]);
+		element->Attribute("pxz", &px[2]);
+		new_object->m_px = make_point(px);
+		element->Attribute("pyx", &py[0]);
+		element->Attribute("pyy", &py[1]);
+		element->Attribute("pyz", &py[2]);
+		new_object->m_py = make_point(py);
+		element->Attribute("pzx", &pz[0]);
+		element->Attribute("pzy", &pz[1]);
+		element->Attribute("pzz", &pz[2]);
+		new_object->m_pz = make_point(pz);
+	}
 
 	// loop through all the objects
 	for(TiXmlElement* pElem = TiXmlHandle(element).FirstChildElement().Element(); pElem; pElem = pElem->NextSiblingElement())
@@ -96,4 +148,188 @@ const wxBitmap &CGroup::GetIcon()
 	static wxBitmap* icon = NULL;
 	if(icon == NULL)icon = new wxBitmap(wxImage(wxGetApp().GetResFolder() + _T("/icons/group.png")));
 	return *icon;
+}
+
+void CGroup::OnEditString(const wxChar* str){
+	m_title.assign(str);
+}
+
+void CGroup::GetGripperPositions(std::list<GripData> *list, bool just_for_endof){
+	if(m_custom_grippers)
+	{
+		if(!m_gripper_datum_set)
+		{
+			CBox box;
+			GetBox(box);
+			if(!box.m_valid)return;
+			m_o = gp_Pnt((box.MinX() + box.MaxX())/2, (box.MinY() + box.MaxY())/2, (box.MinZ() + box.MaxZ())/2);
+			m_px = gp_Pnt(box.MaxX(), (box.MinY() + box.MaxY())/2, (box.MinZ() + box.MaxZ())/2);
+			m_py = gp_Pnt((box.MinX() + box.MaxX())/2, box.MaxY(), (box.MinZ() + box.MaxZ())/2);
+			m_pz = gp_Pnt((box.MinX() + box.MaxX())/2, (box.MinY() + box.MaxY())/2, box.MaxZ());
+			m_gripper_datum_set = true;
+		}
+
+		list->push_back(GripData(GripperTypeTranslate, m_o.X(), m_o.Y(), m_o.Z(), NULL));
+		list->push_back(GripData(GripperTypeRotateObject, m_px.X(), m_px.Y(), m_px.Z(), NULL));
+		list->push_back(GripData(GripperTypeRotateObject, m_py.X(), m_py.Y(), m_py.Z(), NULL));
+		list->push_back(GripData(GripperTypeRotateObject, m_pz.X(), m_pz.Y(), m_pz.Z(), NULL));
+	}
+	else
+	{
+		ObjList::GetGripperPositions(list, just_for_endof);
+	}
+}
+
+static void on_set_custom_grippers(bool value, HeeksObj* object)
+{
+	((CGroup*)object)->m_custom_grippers = value;
+	wxGetApp().Repaint();
+}
+
+static void on_set_o(const double *vt, HeeksObj* object){
+	((CGroup*)object)->m_o = make_point(vt);
+	wxGetApp().Repaint();
+}
+
+static void on_set_px(const double *vt, HeeksObj* object){
+	((CGroup*)object)->m_px = make_point(vt);
+	wxGetApp().Repaint();
+}
+
+static void on_set_py(const double *vt, HeeksObj* object){
+	((CGroup*)object)->m_py = make_point(vt);
+	wxGetApp().Repaint();
+}
+
+static void on_set_pz(const double *vt, HeeksObj* object){
+	((CGroup*)object)->m_pz = make_point(vt);
+	wxGetApp().Repaint();
+}
+
+void CGroup::GetProperties(std::list<Property *> *list)
+{
+	list->push_back ( new PropertyCheck( _("custom grippers"), m_custom_grippers, this, on_set_custom_grippers) );
+
+	if(m_custom_grippers)
+	{
+		double p[3];
+		extract(m_o, p);
+		list->push_back(new PropertyVertex(_("datum"), p, this, on_set_o));
+		extract(m_px, p);
+		list->push_back(new PropertyVertex(_("x"), p, this, on_set_px));
+		extract(m_py, p);
+		list->push_back(new PropertyVertex(_("y"), p, this, on_set_py));
+		extract(m_pz, p);
+		list->push_back(new PropertyVertex(_("z"), p, this, on_set_pz));
+	}
+
+	ObjList::GetProperties(list);
+}
+
+static CGroup* object_for_tools = NULL;
+static gp_Pnt* vertex_for_pick_pos = NULL;
+static void(*callback_for_pick_pos)(const double*) = NULL;
+
+static void on_set_o(const double* pos)
+{
+	gp_Vec shift(object_for_tools->m_o, make_point(pos));
+	object_for_tools->m_o = make_point(pos);
+	object_for_tools->m_px = object_for_tools->m_px.XYZ() + shift.XYZ();
+	object_for_tools->m_py = object_for_tools->m_py.XYZ() + shift.XYZ();
+	object_for_tools->m_pz = object_for_tools->m_pz.XYZ() + shift.XYZ();
+	wxGetApp().Repaint();
+}
+
+static void on_set_pos(const double* pos)
+{
+	*vertex_for_pick_pos = make_point(pos);
+	wxGetApp().Repaint();
+}
+
+class PickPos: public Tool{
+	// Tool's virtual functions
+	void Run(){
+		wxGetApp().CreateUndoPoint();
+		callback_for_pick_pos = on_set_pos;
+		SetVertexPtr();
+		double p[3];
+		extract(*vertex_for_pick_pos, p);
+		wxGetApp().PickPosition(GetTitle(), p, callback_for_pick_pos);
+		wxGetApp().Changed();
+	}
+	wxString BitmapPath(){ return _T("pickpos");}
+	virtual void SetVertexPtr()=0;
+};
+
+class PickO: public PickPos{
+	const wxChar* GetTitle(){return _("Move datum");}
+	void SetVertexPtr(){
+		vertex_for_pick_pos = &object_for_tools->m_o;
+		callback_for_pick_pos = on_set_o;
+	}
+};
+
+class PickPx: public PickPos{
+	const wxChar* GetTitle(){return _("Move px");}
+	void SetVertexPtr(){vertex_for_pick_pos = &object_for_tools->m_px;}
+};
+
+class PickPy: public PickPos{
+	const wxChar* GetTitle(){return _("Move py");}
+	void SetVertexPtr(){vertex_for_pick_pos = &object_for_tools->m_py;}
+};
+
+class PickPz: public PickPos{
+	const wxChar* GetTitle(){return _("Move pz");}
+	void SetVertexPtr(){vertex_for_pick_pos = &object_for_tools->m_pz;}
+};
+
+static PickO pick_o;
+static PickPx pick_px;
+static PickPy pick_py;
+static PickPz pick_pz;
+
+void CGroup::GetTools(std::list<Tool*>* t_list, const wxPoint* p)
+{
+	ObjList::GetTools(t_list, p);
+
+	object_for_tools = this;
+	if(m_custom_grippers)
+	{
+		t_list->push_back(&pick_o);
+		t_list->push_back(&pick_px);
+		t_list->push_back(&pick_py);
+		t_list->push_back(&pick_pz);
+	}
+}
+
+gp_Trsf CGroup::GetMatrix()
+{
+	return make_matrix(m_o, gp_Vec(m_o, m_px), gp_Vec(m_o, m_py));
+}
+
+bool CGroup::GetScaleAboutMatrix(double *m)
+{
+	if(m_custom_grippers)
+	{
+		gp_Trsf mat = GetMatrix();
+		extract(mat, m);
+		return true;
+	}
+	
+	return ObjList::GetScaleAboutMatrix(m);
+}
+
+void CGroup::ModifyByMatrix(const double *m)
+{
+	ObjList::ModifyByMatrix(m);
+
+	if(m_gripper_datum_set)
+	{
+		gp_Trsf mat = make_matrix(m);
+		m_o.Transform(mat);
+		m_px.Transform(mat);
+		m_py.Transform(mat);
+		m_pz.Transform(mat);
+	}
 }
--- src/Group.h
+++ src/Group.h
@@ -10,14 +10,33 @@ class CGroup: public ObjList{
 	std::list<int> m_loaded_solid_ids; // only used during loading xml file
 
 public:
+	wxString m_title;
+	bool m_gripper_datum_set;
+	bool m_custom_grippers;
+	gp_Pnt m_o;
+	gp_Pnt m_px;
+	gp_Pnt m_py;
+	gp_Pnt m_pz;
+
+	CGroup();
+
 	const wxChar* GetTypeString(void)const{return _("Group");}
 	int GetType()const{return GroupType;}
 	HeeksObj *MakeACopy(void)const{ return new CGroup(*this);}
 	const wxBitmap &GetIcon();
 	void WriteXML(TiXmlNode *root);
 	bool UsesID(){return true;}
+	const wxChar* GetShortString(void)const{return m_title.c_str();}
+	bool CanEditString(void)const{return true;}
+	void OnEditString(const wxChar* str);
+	void GetGripperPositions(std::list<GripData> *list, bool just_for_endof);
+	void GetProperties(std::list<Property *> *list);
+	void GetTools(std::list<Tool*>* t_list, const wxPoint* p);
+	bool GetScaleAboutMatrix(double *m);
+	void ModifyByMatrix(const double *m);
 
 	static void MoveSolidsToGroupsById(HeeksObj* object);
+	gp_Trsf GetMatrix();
 
 	static HeeksObj* ReadFromXMLElement(TiXmlElement* pElem);
 };
