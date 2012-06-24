d83d2c83af9d9d6d8c53f7c3fc5a4fa302412c32
Dan Heeks <danheeks@gmail.com>
2012-06-24 20:23:01 +0000

    changed the path to gettext, for compiled "mo" file.

--- translate 3 compile from po to mo.bat
+++ translate 3 compile from po to mo.bat
@@ -1,12 +1,12 @@
 mkdir "%HEEKSCADPATH%\de"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\de\HeeksCAD.mo" "%HEEKSCADPATH%\translations\de\HeeksCAD.po"
+"C:\Users\Dan\Downloads\gettext-0.14.4-bin\bin\msgfmt.exe" -o "%HEEKSCADPATH%\de\HeeksCAD.mo" "%HEEKSCADPATH%\translations\de\HeeksCAD.po"
 mkdir "%HEEKSCADPATH%\it"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\it\HeeksCAD.mo" "%HEEKSCADPATH%\translations\it\HeeksCAD.po"
+"C:\Users\Dan\Downloads\gettext-0.14.4-bin\bin\msgfmt.exe" -o "%HEEKSCADPATH%\it\HeeksCAD.mo" "%HEEKSCADPATH%\translations\it\HeeksCAD.po"
 mkdir "%HEEKSCADPATH%\fr"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\fr\HeeksCAD.mo" "%HEEKSCADPATH%\translations\fr\HeeksCAD.po"
+"C:\Users\Dan\Downloads\gettext-0.14.4-bin\bin\msgfmt.exe" -o "%HEEKSCADPATH%\fr\HeeksCAD.mo" "%HEEKSCADPATH%\translations\fr\HeeksCAD.po"
 mkdir "%HEEKSCADPATH%\sk"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\sk\HeeksCAD.mo" "%HEEKSCADPATH%\translations\sk\HeeksCAD.po"
+"C:\Users\Dan\Downloads\gettext-0.14.4-bin\bin\msgfmt.exe" -o "%HEEKSCADPATH%\sk\HeeksCAD.mo" "%HEEKSCADPATH%\translations\sk\HeeksCAD.po"
 mkdir "%HEEKSCADPATH%\ko"
-"C:\Program Files\GnuWin32\bin\msgfmt.exe" -o "%HEEKSCADPATH%\ko\HeeksCAD.mo" "%HEEKSCADPATH%\translations\ko\HeeksCAD.po"
+"C:\Users\Dan\Downloads\gettext-0.14.4-bin\bin\msgfmt.exe" -o "%HEEKSCADPATH%\ko\HeeksCAD.mo" "%HEEKSCADPATH%\translations\ko\HeeksCAD.po"
 
 pause
