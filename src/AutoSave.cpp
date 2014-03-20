1338e6bce226ec75012bcd9cf617b7df84f17a40
Dan Heeks <danheeks@gmail.com>
2014-03-20 11:50:45 +0000

    supporting wx2.8 ( because I can't work out how to upgrade from 2.8 to 3.0 for Linux )

--- src/AutoSave.cpp
+++ src/AutoSave.cpp
@@ -11,7 +11,12 @@ CAutoSave::CAutoSave(const int interval, const bool skip_recovery /* = false */ 
 	// We need to see if our backup file already exists.  If so, we should
 	// prompt the user to see if they want to use its contents or ignore it.
 
+#if wxCHECK_VERSION(3, 0, 0)
 	wxStandardPaths& standard_paths = wxStandardPaths::Get();
+#else
+	wxStandardPaths standard_paths;
+#endif
+
 	wxFileName path( standard_paths.GetTempDir().c_str(), _(".HeeksCAD_Backup_Data_File.heeks"));
 	m_backup_file_name = path.GetFullPath();
 
