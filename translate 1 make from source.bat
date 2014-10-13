89f961a52d2cff3ac9314549de9b637d16ab5f54
danheeks <danheeks@1e116681-dd50-0410-83da-2bc3025dbda1>
2014-10-13 09:04:04 +0000

    changes while using gettext to translate po files to mo, including adding new languages

--- translate 1 make from source.bat
+++ translate 1 make from source.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\GnuWin32\bin\xgettext.exe" -C -n -k_ -o "C:\Dev\HeeksCAD1.0\HeeksCAD.po" C:\Dev\HeeksCAD1.0\src\*.cpp C:\Dev\HeeksCAD1.0/src/*.h C:\Dev\HeeksCAD1.0\interface\*.cpp C:\Dev\HeeksCAD1.0\interface\*.h C:\Dev\HeeksCNCSVN\src\*.cpp C:\Dev\HeeksCNCSVN/src/*.h
+"C:\Program Files (x86)\GnuWin32\bin\xgettext.exe" -C -n -k_ -o "C:\Dev\HeeksCAD1.0\HeeksCAD.po" C:\Dev\HeeksCAD1.0\src\*.cpp C:\Dev\HeeksCAD1.0/src/*.h C:\Dev\HeeksCAD1.0\interface\*.cpp C:\Dev\HeeksCAD1.0\interface\*.h C:\Dev\HeeksCNC1.0\src\*.cpp C:\Dev\HeeksCNC1.0/src/*.h
 
 pause
